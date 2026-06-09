d01866f9ede8c0339202d30adefae07903debfe5
Eman Raz <121406187+Emanoel91@users.noreply.github.com>
2026-06-09 04:14:58 +0330

    Update 1_⛓Chain_Stats.py

--- pages/1_⛓Chain_Stats.py
+++ pages/1_⛓Chain_Stats.py
@@ -248,8 +248,8 @@ logo = selected_chain["logo"]
 
 st.markdown(
     f"""
-    <div style="display:flex;align-items:center;gap:20px;margin-bottom:25px;">
-        <img src="{logo}" width="10">
+    <div style="display:flex;align-items:center;gap:20px;margin-bottom:100px;">
+        <img src="{logo}" width="50">
         <div>
             <h2 style="margin:0;">{chain_name}</h2>
             <p style="margin:0;font-size:18px;">
